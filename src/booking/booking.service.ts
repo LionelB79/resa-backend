0a60b34035f2587510d069f0210d486a1ba67067
Lionel <bachmannlionel@gmail.com>
2024-11-30 16:29:39 +0100

    ajout entités au module + ajout service creation de reservation

--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BookingEntity } from '@booking/booking.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class BookingService {}
+export class BookingService {
+  constructor(
+    @InjectRepository(BookingEntity)
+    private bookingRepository: Repository<BookingEntity>,
+  ) {}
+
+  async createBooking(
+    bookingTitle: string,
+    userEmail: string,
+    roomId: string,
+    startTime: Date,
+    endTime: Date,
+  ): Promise<BookingEntity> {
+    const booking = this.bookingRepository.create({
+      roomId: roomId,
+      bookingTitle: bookingTitle,
+      userEmail: userEmail,
+      startTime,
+      endTime,
+    });
+
+    return await this.bookingRepository.save(booking);
+  }
+}
