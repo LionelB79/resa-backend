0ebae400a30349f1bc9a97b2b407385fd34491a7
Lionel <bachmannlionel@gmail.com>
2024-11-30 11:43:34 +0100

    ajout controleur createRoom

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateRoomDto } from '@room/dtos/create-room.dto';
+import { RoomEntity } from '@room/room.entity';
+import { RoomService } from './room.service';
 
 @Controller('room')
-export class RoomController {}
+export class RoomController {
+  constructor(private readonly roomService: RoomService) {}
+
+  @Post('/createroom')
+  async createRoom(@Body() createRoomDto: CreateRoomDto): Promise<RoomEntity> {
+    return this.roomService.createRoom(createRoomDto);
+  }
+}
