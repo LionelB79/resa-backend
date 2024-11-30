2233ab75377bc675fe4d28c2a45b549671da70b0
Lionel <bachmannlionel@gmail.com>
2024-11-30 01:13:08 +0100

    ajout connexion bdd + ajout entités au AppModule + ajout relativePath

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RoomModule } from './room/room.module';
-import { BookingModule } from './booking/booking.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoomModule } from '@room/room.module';
+import { RoomEntity } from '@room/room.entity';
+import { BookingModule } from '@booking/booking.module';
+import { BookingEntity } from '@booking/booking.entity';
 
 @Module({
-  imports: [RoomModule, BookingModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost:27017/resa',
+      useUnifiedTopology: true,
+      synchronize: true, // Synchronisation automatique des schémas (!!!! A désactiver en prod !!!!)
+      entities: [RoomEntity, BookingEntity],
+    }),
+    RoomModule,
+    BookingModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
