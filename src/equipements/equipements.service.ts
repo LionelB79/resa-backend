3c2745b4f5caf02e66abd8d83b6e9ff6655fb3d5
Lionel <bachmannlionel@gmail.com>
2024-12-02 10:33:31 +0100

    ajout service de creation d'equipement

--- src/equipements/equipements.service.ts
+++ src/equipements/equipements.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { EquipementsEntity } from '@equipements/equipements.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class EquipementsService {}
+export class EquipementsService {
+  constructor(
+    @InjectRepository(EquipementsEntity)
+    private equipementsRepository: Repository<EquipementsEntity>,
+  ) {}
+
+  async createEquipment(name: string): Promise<EquipementsEntity> {
+    // Vérifie si l'équipement existe déjà
+    let equipment = await this.equipementsRepository.findOne({
+      where: { name },
+    });
+    if (!equipment) {
+      // Crée un nouvel équipement s'il n'existe pas
+      equipment = this.equipementsRepository.create({ name });
+      await this.equipementsRepository.save(equipment);
+    }
+    return equipment;
+  }
+}
