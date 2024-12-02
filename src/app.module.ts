27b051d41e52c98682c5add77f3c3fa029e0759c
Lionel <bachmannlionel@gmail.com>
2024-12-02 11:12:17 +0100

    modif roomEntity + ajout equipementEnity au module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,8 @@ import { RoomModule } from '@room/room.module';
 import { RoomEntity } from '@room/room.entity';
 import { BookingModule } from '@booking/booking.module';
 import { BookingEntity } from '@booking/booking.entity';
-import { EquipementsModule } from './equipements/equipements.module';
+import { EquipementsModule } from '@equipements/equipements.module';
+import { EquipementsEntity } from '@equipements/equipements.entity';
 
 @Module({
   imports: [
@@ -15,7 +16,7 @@ import { EquipementsModule } from './equipements/equipements.module';
       url: 'mongodb://localhost:27017/resa',
       useUnifiedTopology: true,
       synchronize: true, // Synchronisation automatique des schémas (!!!! A désactiver en prod !!!!)
-      entities: [RoomEntity, BookingEntity],
+      entities: [RoomEntity, BookingEntity, EquipementsEntity],
     }),
     RoomModule,
     BookingModule,
