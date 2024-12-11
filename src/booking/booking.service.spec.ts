220d3f5b214d1a257d7e86b1f0689a73d7f24f44
Lionel <bachmannlionel@gmail.com>
2024-12-12 00:38:49 +0100

    préparation fichier TU pour BookingService

--- src/booking/booking.service.spec.ts
+++ src/booking/booking.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookingService } from './booking.service';
+import { BookingEntity } from './booking.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { RoomEntity } from '../room/room.entity';
 
 describe('BookingService', () => {
   let service: BookingService;
+  let fakeBookingRepository: Partial<Repository<BookingEntity>>;
+  let fakeRoomRepository: Partial<Repository<RoomEntity>>;
 
   beforeEach(async () => {
+    fakeBookingRepository = {};
+    fakeRoomRepository = {};
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BookingService],
+      providers: [
+        BookingService,
+        {
+          provide: getRepositoryToken(BookingEntity),
+          useValue: fakeBookingRepository,
+        },
+        {
+          provide: getRepositoryToken(RoomEntity),
+          useValue: fakeRoomRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<BookingService>(BookingService);
